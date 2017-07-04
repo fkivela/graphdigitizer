3b52e5969340d118e9cf620a23eba2f7d4f7a766
Feliks <feliks.kivela@elisanet.fi>
2017-07-04 10:55:45 +0300

    Improve algorithm: find middle of thick line

--- graph/findgraph.m
+++ graph/findgraph.m
@@ -53,6 +53,44 @@ end
 end
 
 
+% function [graphIndices, smallestDifferences] = indicesFromDifferences(startI, differences, colorParam)
+% 
+% ySize = size(differences, 1);
+% xSize = size(differences, 2);
+% 
+% graphIndices = zeros(1, xSize);
+% smallestDifferences = graphIndices;
+% 
+% scores = zeros(ySize, xSize);
+% 
+% for j = 1:xSize
+% 
+%     for i = 1:ySize
+%         colorDiff = 1000 * differences(i, j) / sqrt(3);
+%         
+%         if j == 1
+%             newI = startI;
+%         elseif j == 2
+%             newI = 2 * graphIndices(j - 1) - startI;
+%         else
+%             newI = 2 * graphIndices(j - 1) - graphIndices(j - 2);
+%         end
+%    
+%         shapeDiff = 1000 * abs(i - newI) / ySize;
+%         scores(i, j) = colorParam * colorDiff + (1-colorParam) * shapeDiff;
+% 
+%     end
+%     
+%     column = scores(:,j);
+%     [~, sortedIndices] = sort(column);
+%     bestI = sortedIndices(1);
+%     graphIndices(j) = bestI;
+%     smallestDifferences(j) = column(bestI);
+% 
+% end
+% 
+% end
+
 function [graphIndices, smallestDifferences] = indicesFromDifferences(startI, differences, colorParam)
 
 ySize = size(differences, 1);
@@ -66,7 +104,7 @@ scores = zeros(ySize, xSize);
 for j = 1:xSize
 
     for i = 1:ySize
-        colorDiff = 1000 * differences(i, j) / 255;
+        colorDiff = 1000 * differences(i, j) / sqrt(3);
         
         if j == 1
             newI = startI;
@@ -88,5 +126,50 @@ for j = 1:xSize
     smallestDifferences(j) = column(bestI);
 
 end
+
+
+for j = 1:xSize
+    i0 = graphIndices(j);
+
+    list = [i0 differences(i0, j)];
+    
+    for i = i0 + 1 : i0 + ySize / 20
+        if i < 1 || i > ySize
+            continue
+        end
+                
+        if differences(i, j) > sqrt(3) / 2
+            break
+        end
+        
+        list = cat(1, list, [i, differences(i, j)]);
+    end
+    
+    for i = i0 - 1 : -1 : i0 - ySize / 20
+        if i < 1 || i > ySize
+            continue
+        end
+                
+        if differences(i, j) > sqrt(3) / 2
+            break
+        end
+        
+        list = cat(1, list, [i, differences(i, j)]);
+    end
+    
+    sum = 0;
+    colorsum = 0;
+    for i = 1:size(list, 1)
+        sum = sum + list(i, 1) * (sqrt(3) - list(i, 2));
+        colorsum = colorsum + (sqrt(3) - list(i, 2));
+    end
+    
+    graphIndices(j) = round(sum / colorsum);
+    
+%     if j < 20
+%         j
+%         list
+%     end
+end    
 
 end
