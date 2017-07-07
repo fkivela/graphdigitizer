3e4b0aa8078963a7b3ec4dfb512583bae203d88b
Feliks <feliks.kivela@elisanet.fi>
2017-07-07 13:13:19 +0300

    Improve graph finding for light colored graphs

--- graph/findgraph.m
+++ graph/findgraph.m
@@ -27,10 +27,12 @@ y = graphIndices;
 function [graphIndices, smallestDifferences] = findIndices()
 
 %Calculate color differences between graph and pixels
+colors = double(I);
 differences = zeros(ySize, xSize);
+
 for i = 1:ySize
     for j = 1:xSize
-        color = double([I(i, j, 1), I(i, j, 2), I(i, j, 3)]);      
+        color = [colors(i, j, 1), colors(i, j, 2), colors(i, j, 3)];
         difference = norm(color - graphColor);
         differences(i, j) = difference;
     end
@@ -40,11 +42,11 @@ graphIndices = zeros(1, xSize);
 smallestDifferences = 999 * ones(1, xSize);
 
 %Find i-indices of graph points using differences
-[indicesEnd, diffEnd] = indicesFromDifferences(startPointI, differences(:, startPointJ : xSize), colorParam);
+[indicesEnd, diffEnd] = indicesFromDifferences(startPointI, colors, differences(:, startPointJ : xSize), colorParam);
 graphIndices(startPointJ : xSize) = indicesEnd;
 smallestDifferences(startPointJ : xSize) = diffEnd;
 
-[indicesStart, diffStart] = indicesFromDifferences(startPointI, differences(:, startPointJ: -1 : 1), colorParam);
+[indicesStart, diffStart] = indicesFromDifferences(startPointI, colors, differences(:, startPointJ: -1 : 1), colorParam);
 graphIndices(startPointJ : -1 : 1) = indicesStart;
 smallestDifferences(startPointJ : -1 : 1) = diffStart;
 
@@ -52,46 +54,7 @@ end
 
 end
 
-
-% function [graphIndices, smallestDifferences] = indicesFromDifferences(startI, differences, colorParam)
-% 
-% ySize = size(differences, 1);
-% xSize = size(differences, 2);
-% 
-% graphIndices = zeros(1, xSize);
-% smallestDifferences = graphIndices;
-% 
-% scores = zeros(ySize, xSize);
-% 
-% for j = 1:xSize
-% 
-%     for i = 1:ySize
-%         colorDiff = 1000 * differences(i, j) / sqrt(3);
-%         
-%         if j == 1
-%             newI = startI;
-%         elseif j == 2
-%             newI = 2 * graphIndices(j - 1) - startI;
-%         else
-%             newI = 2 * graphIndices(j - 1) - graphIndices(j - 2);
-%         end
-%    
-%         shapeDiff = 1000 * abs(i - newI) / ySize;
-%         scores(i, j) = colorParam * colorDiff + (1-colorParam) * shapeDiff;
-% 
-%     end
-%     
-%     column = scores(:,j);
-%     [~, sortedIndices] = sort(column);
-%     bestI = sortedIndices(1);
-%     graphIndices(j) = bestI;
-%     smallestDifferences(j) = column(bestI);
-% 
-% end
-% 
-% end
-
-function [graphIndices, smallestDifferences] = indicesFromDifferences(startI, differences, colorParam)
+function [graphIndices, smallestDifferences] = indicesFromDifferences(startI, colors, differences, colorParam)
 
 ySize = size(differences, 1);
 xSize = size(differences, 2);
@@ -127,6 +90,11 @@ for j = 1:xSize
 
 end
 
+%stopcondition = @(i, j) (differences(i, j) > sqrt(3) / 2) || (sum(colors(i,j,1:3),3) < sqrt(3) / 10);
+%stopcondition = @(i, j) (differences(i, j) > sqrt(3) / 2) || (sum(colors(i,j,1:3),3) < 0.3);
+%stopcondition = @(i, j) (differences(i, j) > sqrt(3) / 2) || true;
+stopcondition = @(i, j) (differences(i, j) > sqrt(3) / 5);
+
 
 for j = 1:xSize
     i0 = graphIndices(j);
@@ -137,8 +105,8 @@ for j = 1:xSize
         if i < 1 || i > ySize
             continue
         end
-                
-        if differences(i, j) > sqrt(3) / 2
+        
+        if stopcondition(i, j)
             break
         end
         
@@ -150,26 +118,26 @@ for j = 1:xSize
             continue
         end
                 
-        if differences(i, j) > sqrt(3) / 2
+        if stopcondition(i, j)
             break
         end
         
         list = cat(1, list, [i, differences(i, j)]);
     end
     
-    sum = 0;
+    total = 0;
     colorsum = 0;
     for i = 1:size(list, 1)
-        sum = sum + list(i, 1) * (sqrt(3) - list(i, 2));
+        total = total + list(i, 1) * (sqrt(3) - list(i, 2));
         colorsum = colorsum + (sqrt(3) - list(i, 2));
     end
     
-    graphIndices(j) = round(sum / colorsum);
+    graphIndices(j) = round(total / colorsum);
     
-%     if j < 20
-%         j
-%         list
-%     end
+     %if j < 5
+     %    j
+     %    list
+     %end
 end    
 
 end
